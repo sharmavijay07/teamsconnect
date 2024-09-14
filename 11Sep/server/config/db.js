120a72e2e455cc7c0b9d0f2fd13d436940cf70fc
cosmicrolex <nipunmahajan425201@gmail.com>
2024-09-14 10:27:20 +0530

    this is commit time: 10:27

--- 11Sep/server/config/db.js
+++ 11Sep/server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'123456',
+    password:'Cosmicrolex@123',
     database:'teamsconnect'
 })
 
