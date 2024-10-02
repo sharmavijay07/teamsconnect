4094cae2865630aefe08ab87feffb045c1f403a6
LevazeDigital <147309264+LevazeDigital@users.noreply.github.com>
2024-10-02 14:50:52 +0530

    Your commit message

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host:'localhost',
+    host: 'localhost',
     user:'root',
     password:'123456', 
     // nipun ka password:
