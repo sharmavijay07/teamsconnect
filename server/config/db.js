443ef8fc7a1dcb8e6fd361b23544bc737ebef9f2
LevazeDigital <147309264+LevazeDigital@users.noreply.github.com>
2024-10-02 15:22:34 +0530

    New commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,24 +1,18 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+require('dotenv').config();  // Load environment variables from .env file
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'123456', 
-    // nipun ka password:
-    // password:'12345678', 
-    
-    // password:'123456', 
-    
-    //password:'suravijay'
-    // password: 'Shantanu@2104',
-   database:'teamsconnect'
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
 db.connect((err) => {
     if(err)
-        console.log('error: ',err)
+        console.log('error: ', err);
     else
-        console.log('connected')
-})
+        console.log('connected');
+});
 
 module.exports = db;
